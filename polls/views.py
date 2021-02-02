6536ab48301ec3ba6a9d80cfd9c3d454dacd217b
hughbagan <25750667+hughbagan@users.noreply.github.com>
2021-02-01 17:54:19 -0700

    Made index, detail, and results Generic Views; answered question 8

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    # ListView: display a list of objects
+    template_name = 'polls/index.html'
+    # default context_object_name: 'question_list'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+class DetailView(generic.DetailView):
+    # DetailView: display a detail page for a particular type of object
+    # Expects the primary key value captured from URL to be called 'pk'
+    # default template: <appname>/<modelname>_detail.html
+    model = Question # the model being acted upon
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -31,11 +39,10 @@ def vote(request, question_id):
             'question': question,
             'error_message': "You didn't select a choice.",
         })
-    else: # ???
+    else:
         selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing with
         # POST data. This prevents data from being posted twice if a user 
         # hits the Back button.
-        # 'polls:results'
-        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
